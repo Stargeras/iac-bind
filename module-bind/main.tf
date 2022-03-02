8ad8a4df8cefd0bbe7224f6708b8ade9d4b17d5c
Stargeras <dillongrosso@live.com>
2022-03-02 16:08:01 -0500

    terraform fmt

--- module-bind/main.tf
+++ module-bind/main.tf
@@ -5,116 +5,116 @@ provider "kubernetes" {
 }
 
 resource "kubernetes_namespace" "bind" {
-    count = var.bind_namespace != "default" ? 1 : 0
-    metadata {
-        name = "${var.bind_namespace}"
-    }
+  count = var.bind_namespace != "default" ? 1 : 0
+  metadata {
+    name = var.bind_namespace
+  }
 }
 
 resource "kubernetes_config_map" "named-conf" {
-    metadata {
-        name = "named-conf"
-        namespace = "${var.bind_namespace}"
-    }
-    data = {
-        config = "${file("${path.root}/named.conf")}"
-    }
-    depends_on = [kubernetes_namespace.bind]
+  metadata {
+    name      = "named-conf"
+    namespace = var.bind_namespace
+  }
+  data = {
+    config = "${file("${path.root}/named.conf")}"
+  }
+  depends_on = [kubernetes_namespace.bind]
 }
 
 resource "kubernetes_config_map" "records-db" {
-    metadata {
-        name = "records-db"
-        namespace = "${var.bind_namespace}"
-    }
-    data = {
-        config = "${file("${path.root}/records.db")}"
-    }
-    depends_on = [kubernetes_namespace.bind]
+  metadata {
+    name      = "records-db"
+    namespace = var.bind_namespace
+  }
+  data = {
+    config = "${file("${path.root}/records.db")}"
+  }
+  depends_on = [kubernetes_namespace.bind]
 }
 
 resource "kubernetes_deployment" "bind" {
-    metadata {
-        name = "bind"
-        namespace = "${var.bind_namespace}"
+  metadata {
+    name      = "bind"
+    namespace = var.bind_namespace
+    labels = {
+      app = "bind"
+    }
+  }
+  spec {
+    replicas = var.bind_replicas
+    selector {
+      match_labels = {
+        app = "bind"
+      }
+    }
+    template {
+      metadata {
         labels = {
-            app = "bind"
+          app = "bind"
         }
+      }
+      spec {
+        container {
+          image = var.bind_image
+          name  = "bind"
+          volume_mount {
+            name       = "named-conf"
+            mount_path = "/etc/bind/named.conf"
+            sub_path   = "named.conf"
+          }
+          volume_mount {
+            name       = "records-db"
+            mount_path = "/etc/bind/records.db"
+            sub_path   = "records.db"
+          }
+        }
+        volume {
+          name = "named-conf"
+          config_map {
+            name = "named-conf"
+            items {
+              key  = "config"
+              path = "named.conf"
+            }
+          }
+        }
+        volume {
+          name = "records-db"
+          config_map {
+            name = "records-db"
+            items {
+              key  = "config"
+              path = "records.db"
+            }
+          }
+        }
+      }
     }
-    spec {
-        replicas = "${var.bind_replicas}"
-        selector {
-            match_labels = {
-                app = "bind"
-            }
-        }
-        template {
-            metadata {
-                labels = {
-                    app = "bind"
-                }
-            }
-            spec {
-                container {
-                    image = "${var.bind_image}"
-                    name  = "bind"
-                    volume_mount {
-                        name = "named-conf"
-                        mount_path = "/etc/bind/named.conf"
-                        sub_path = "named.conf"
-                    }
-                    volume_mount {
-                        name = "records-db"
-                        mount_path = "/etc/bind/records.db"
-                        sub_path = "records.db"
-                    }
-                }
-                volume {
-                    name = "named-conf"
-                    config_map {
-                        name = "named-conf"
-                        items {
-                            key = "config"
-                            path = "named.conf"
-                        }
-                    }
-                }
-                volume {
-                    name = "records-db"
-                    config_map {
-                        name = "records-db"
-                        items {
-                            key = "config"
-                            path = "records.db"
-                        }
-                    }
-                }
-            }
-        }
-    }
-    depends_on = [
-        kubernetes_namespace.bind,
-        kubernetes_config_map.named-conf,
-        kubernetes_config_map.records-db,
-    ]
+  }
+  depends_on = [
+    kubernetes_namespace.bind,
+    kubernetes_config_map.named-conf,
+    kubernetes_config_map.records-db,
+  ]
 }
 
 resource "kubernetes_service" "bind" {
-    metadata {
-        name = "bind"
-        namespace = "${var.bind_namespace}"
+  metadata {
+    name      = "bind"
+    namespace = var.bind_namespace
+  }
+  spec {
+    selector = {
+      app = "bind"
     }
-    spec {
-        selector = {
-            app = "bind"
-        }
-        port {
-            port        = 53
-            target_port = 53
-            protocol = "UDP"
-            name = "bind-udp"
-        }
-        type = "LoadBalancer"
+    port {
+      port        = 53
+      target_port = 53
+      protocol    = "UDP"
+      name        = "bind-udp"
     }
-    depends_on = [kubernetes_namespace.bind]
+    type = "LoadBalancer"
+  }
+  depends_on = [kubernetes_namespace.bind]
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "kubeconfig" {
-    type = string
-    default = "~/.kube/config"
+  type    = string
+  default = "~/.kube/config"
 }
 
 variable "bind_image" {
-    type = string
-    default = "ubuntu/bind9"
+  type    = string
+  default = "ubuntu/bind9"
 }
 variable "bind_namespace" {
-    type = string
-    default = "bind"
+  type    = string
+  default = "bind"
 }
 variable "bind_replicas" {
-    type = number
-    default = 1
+  type    = number
+  default = 1
 }
